5285cc78c59038427dbe1032d7a17cbfe9494ab8
amit <amittibi123@gmail.com>
2026-02-28 13:19:00 +0200

    adding email registration and the to do page

--- set.Web/Program.cs
+++ set.Web/Program.cs
@@ -1,11 +1,15 @@
 using set.Web.Components;
 using set.Shared.Components; // הוספנו את זה
+using Blazored.LocalStorage;  // הוספנו את השורה הזו
 
 var builder = WebApplication.CreateBuilder(args);
 
 // הוספת שירותי Blazor
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// הוספת Blazored LocalStorage לשירותי התלותים
+builder.Services.AddBlazoredLocalStorage();
 
 var app = builder.Build();
 
