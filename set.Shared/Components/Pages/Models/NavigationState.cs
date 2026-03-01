16f2651b2455d2a8ed8a2439f25a2defc9076476
amit <amittibi123@gmail.com>
2026-03-01 19:15:59 +0200

    added a nav to settings

---
+++ set.Shared/Components/Pages/Models/NavigationState.cs
@@ -0,0 +1,15 @@
+public class NavigationState
+{
+    public bool IsInSettings { get; private set; }
+
+    // אירוע שמעדכן את התפריט כשמשהו משתנה
+    public event Action OnChange;
+
+    public void SetSettingsMode(bool value)
+    {
+        IsInSettings = value;
+        NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
