0fc5cf07748cac44b2eb24f204466dbbb79554e1
amit <amittibi123@gmail.com>
2026-03-01 16:45:36 +0200

    added a more leng and fix bugs

--- set.Shared/Components/Pages/Models/LanguageService.cs
+++ set.Shared/Components/Pages/Models/LanguageService.cs
@@ -3,21 +3,16 @@ using Microsoft.Extensions.Localization;
 
 public class LanguageService
 {
-    // אירוע שמופעל בכל פעם שהשפה משתנה
+    
     public event Action? OnLanguageChanged;
 
-    public LanguageService()
-    {
-        // הגדרת אנגלית כברירת מחדל בבנייה של השירות
-        SetLanguage("en-US");
-    }
     public void SetLanguage(string cultureCode)
     {
         var culture = new CultureInfo(cultureCode);
         CultureInfo.DefaultThreadCurrentCulture = culture;
         CultureInfo.DefaultThreadCurrentUICulture = culture;
 
-        // מודיעים לכל מי שמקשיב שהשפה השתנתה
+        
         OnLanguageChanged?.Invoke();
     }
 
